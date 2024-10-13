2faaa069a1b26387a56e0748e091a8b63da449b2
veto <veto@myridia.com>
2024-10-13 10:21:55 +0700

    add form data

--- Willdo/SettingView.swift
+++ Willdo/SettingView.swift
@@ -19,83 +19,34 @@ struct SettingView: View {
     @AppStorage("user") var user = ""
     @AppStorage("password") var password = ""
     @AppStorage("couchdb") var couchdb = false
-    @AppStorage("iscouchdb") var iscouchdb = false
-
+    @AppStorage("sync_data") var sync_data = false
+   // @AppStorage("userInput") var sync_data = ""
     @AppStorage("languageIndex") var languageIndex = 0
     //@State private var username = ""
     //@State private var password = ""
     @AppStorage("index") var index = 0
     var body: some View {
         NavigationView {
-       
             
+            //https://www.codespeedy.com/read-text-from-a-textfield-in-swiftui/
             Form {
-                        Section(header: Text("Notifications")) {
-                  
-                            Toggle("Play notification sounds", isOn: $iscouchdb)
-                        }
-                        
-                    }
-            
-            
-            SettingStack {
-                /// This is the main settings page.
-                SettingPage(title: "Settings") {
-      
-                    /// Use groups to group components together.
-                    SettingGroup(header: "Online Storage") {
-                        SettingToggle(title: "Save Data to Couchdb", isOn: $couchdb)
-                        if couchdb  {
-                            SettingTextField( placeholder: "Enter couchdb host", text: $host)
-                      
-                            SettingTextField( placeholder: "Enter User", text: $user)
-                            
-                            SettingTextField( placeholder: "Enter Password", text: $password)
-                        }
-                    }
+                Section(header: Text("CouchDB Sync")) {
                     
-                    SettingGroup {
-                                     
-                                        SettingPicker(
-                                            title: "Picker with menu",
-                                            choices: ["A", "B", "C", "D"],
-                                            selectedIndex: $index,
-                                            choicesConfiguration: .init(
-                                                pickerDisplayMode: .menu
-                                            )
-                                        )
-                                    }
+                    Toggle("Sync Data", isOn: $sync_data)
                     
-                    
-                   
-                    
-                    
-                    
-                    
-                    
-                    SettingGroup {
-                                               SettingButton(title: "View on GitHub") {
-                                                 
-                                                   
-                                                   
-                                               }
-
-                                               
-                                           }
-                    
-                    
-                    
+               
+                    TextField("Type here", text: $host)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
                 }
                 
                 
             }
             
             
-            
-            
-            
         }
-    }
+            }
+    
     
     
 }
